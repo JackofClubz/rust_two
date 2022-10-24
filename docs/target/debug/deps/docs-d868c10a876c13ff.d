e2de1e5bc9dccbf8cc86afbd712c1c79018c4edb
JackofClubz <lancetersou@gmail.com>
2022-10-24 06:16:01 +0100

    Main

--- docs/target/debug/deps/docs-d868c10a876c13ff.d
+++
@@ -1,5 +0,0 @@
-/Users/lance/rust_two/docs/target/debug/deps/docs-d868c10a876c13ff.rmeta: src/main.rs
-
-/Users/lance/rust_two/docs/target/debug/deps/docs-d868c10a876c13ff.d: src/main.rs
-
-src/main.rs:
--- idiomatic/target/debug/deps/idiomatic-8e2a2040036d75f4.d
+++
@@ -1,7 +0,0 @@
-/Users/lance/rust_two/idiomatic/target/debug/deps/idiomatic-8e2a2040036d75f4.rmeta: src/main.rs
-
-/Users/lance/rust_two/idiomatic/target/debug/deps/idiomatic-8e2a2040036d75f4.d: src/main.rs
-
-src/main.rs:
-
-# env-dep:CLIPPY_ARGS=
