5509371b5c7a80225655a8413ed54d2621fea582
JackofClubz <lancetersou@gmail.com>
2022-10-25 08:02:46 +0100

    Main

---
+++ docs/target/debug/deps/docs-72743eec16ac682a.d
@@ -0,0 +1,5 @@
+/Users/lance/rust_two/docs/target/debug/deps/docs-72743eec16ac682a.rmeta: src/lib.rs
+
+/Users/lance/rust_two/docs/target/debug/deps/docs-72743eec16ac682a.d: src/lib.rs
+
+src/lib.rs:
