ed70ef96d8ea8646ad95ffe8a1dcbfc842731077
JackofClubz <lancetersou@gmail.com>
2022-11-05 23:18:57 +0000

    Main removing makerfile

--- idiomatic/target/debug/deps/idiomatic-31efea66df48ac78.d
+++
@@ -1,5 +0,0 @@
-/Users/lance/rust_two/idiomatic/target/debug/deps/idiomatic-31efea66df48ac78.rmeta: src/main.rs
-
-/Users/lance/rust_two/idiomatic/target/debug/deps/idiomatic-31efea66df48ac78.d: src/main.rs
-
-src/main.rs:
--- traits/target/debug/deps/traits-e2c9a45190f9d7ed.d
+++
@@ -1,5 +0,0 @@
-/Users/lance/rust_two/traits/target/debug/deps/traits-e2c9a45190f9d7ed.rmeta: src/main.rs
-
-/Users/lance/rust_two/traits/target/debug/deps/traits-e2c9a45190f9d7ed.d: src/main.rs
-
-src/main.rs:
